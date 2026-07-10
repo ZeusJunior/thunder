41558950a06102944c2af55a5956444c2a41bc79
ZeusJunior <32263615+ZeusJunior@users.noreply.github.com>
2026-07-11 00:25:09 +0200

    chore: spacing

--- main/helpers/request.ts
+++ main/helpers/request.ts
@@ -2,8 +2,8 @@ import request from 'request';
 
 // As of 11 Jul 2026, Steam seems to 429 requests that don't have some kind of browser header
 export default request.defaults({
-    headers: {
-        'Accept-Encoding': 'gzip, deflate, br, zstd',
-        'accept-language': 'en-US,en;q=0.9',
-    }
+  headers: {
+    'Accept-Encoding': 'gzip, deflate, br, zstd',
+    'accept-language': 'en-US,en;q=0.9',
+  }
 });
